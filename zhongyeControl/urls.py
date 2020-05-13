991bd1ba2d4be353e157bde37eed6b0e84099687
anding <1506032039@qq.com>
2020-05-13 19:57:40 +0800

    更新

--- zhongyeControl/urls.py
+++ zhongyeControl/urls.py
@@ -1,28 +1,31 @@
 # -*- coding: utf-8 -*-
-#project:zhongye
-#author: Administrator
-#contact: 1506032039@qq.com
-#software: PyCharm
-#file: urls.py
-#time: 2019/5/2 14:38
+# project:zhongye
+# author: Administrator
+# contact: 1506032039@qq.com
+# software: PyCharm
+# file: urls.py
+# time: 2019/5/2 14:38
 
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index),
-    url(r'^login/$', views.login, name = 'login'),
-    url(r'zhongye/$', views.first, name = 'first'),
-    url(r'approval_index/$',views.approval_index,name="approval_index"),
-    url(r'zhongye/approval_report/$',views.approval_report,name="approval_report"),
-    url(r'zhongye/assignment/$',views.assignment,name="assignment"),
-    url(r'assignment_index/$',views.assignment_index,name="assignment_index"),
-    url(r'consignment/$',views.consignment,name="consignment"),
-    url(r'zhongye/experiment/$',views.experiment,name="experiment"),
-    url(r'sample_index/$',views.sample_index,name="sample_index"),
-    url(r'test_index/$',views.test_index,name="test_index"),
-    url(r'zhongye/test/$',views.test,name="test"),
-    url(r'^LoadData/$', views.LoadData, name = 'LoadData'),
+    url(r'^$', views.index),
+    url(r'^login/$', views.login, name='login'),
+    url(r'zhongye/$', views.first, name='first'),
+    url(r'approval_index/$', views.approval_index, name="approval_index"),
+    url(r'zhongye/approval_report/$', views.approval_report, name="approval_report"),
+    url(r'zhongye/assignment/$', views.assignment, name="assignment"),
+    url(r'assignment_index/$', views.assignment_index, name="assignment_index"),
+    url(r'assignment/$', views.assignment, name="assignment"),
+    # url(r'toConsignment/$',views.toConsignment,name='toConsignment'),
+    url(r'consignment/$', views.consignment, name="consignment"),
+    url(r'zhongye/experiment/$', views.experiment, name="experiment"),
+    url(r'sample_index/$', views.sample_index, name="sample_index"),
+    url(r'samIndexData/$', views.samIndexData, name='LoadData'),
+    url(r'test_index/$', views.test_index, name="test_index"),
+    url(r'zhongye/test/$', views.test, name="test"),
+    url(r'^LoadData/$', views.LoadData, name='LoadData'),
 
     url(r'^loadOriginalRecord/$', views.loadOriginalRecord, name='loadOriginalRecord'),
 ]
--- zhongyeControl/views.py
+++ zhongyeControl/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.http import HttpResponseRedirect
 from .decoBean import CheckSession
 from django.core import serializers
@@ -12,35 +12,61 @@ import time
 import os
 import re
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'zhongye/Login.html')
 
+
+@CheckSession
 def approval_index(request):
     return render(request, 'zhongye/approval_index.html')
 
+
+@CheckSession
 def approval_report(request):
     return render(request, 'zhongye/approval_report.html')
 
+@csrf_exempt  # 增加装饰器，作用是跳过 csrf 中间件的保护
+@CheckSession
 def assignment(request):
     return render(request, 'zhongye/assignment.html')
 
+
+@CheckSession
 def assignment_index(request):
     return render(request, 'zhongye/assignment_index.html')
 
-def consignment(request):
-    return render(request, 'zhongye/consignment.html')
 
+
+
+
+@csrf_exempt  # 增加装饰器，作用是跳过 csrf 中间件的保护
+@CheckSession
+def consignment(request):
+    # return render(request, 'zhongye/consignment.html')
+    # if request.method == "AJAX":
+    sample_id = request.POST.get("sample_id")
+    return render(request, 'zhongye/consignment.html',{"sample_id":sample_id})
+
+
+@CheckSession
 def experiment(request):
     return render(request, 'zhongye/experiment.html')
 
+
+@CheckSession
 def sample_index(request):
     return render(request, 'zhongye/sample_index.html')
 
+
+@CheckSession
 def test_index(request):
     return render(request, 'zhongye/test_index.html')
 
+
+@CheckSession
 def test(request):
     return render(request, 'zhongye/test.html')
 
@@ -54,7 +80,7 @@ def login(request):  # 验证登录信息
         if user.count() > 0:
             user = user.first()
             if user.pwd == userpwd:
-                LoginSession = {'LoginName': username, 'LoginId': user.id,'LoginCx':user.cx }
+                LoginSession = {'LoginName': username, 'LoginId': user.id, 'LoginCx': user.cx}
                 request.session['LoginSession'] = LoginSession
                 request.session.set_expiry(0)
                 # return render(request, 'zhongye/professorTable.html')
@@ -73,17 +99,39 @@ def login(request):  # 验证登录信息
     elif request.method == 'GET':
         return render(request, 'zhongye/login1.html')
 
+
 @CheckSession
 def first(request):
-    if  request.session.get('LoginSession')['LoginCx'] == 1:
+    if request.session.get('LoginSession')['LoginCx'] == 1:
         return render(request, 'zhongye/approval_index.html')
     else:
         return render(request, 'zhongye/login.html')
 
 
+@csrf_exempt
+def samIndexData(request):
+    datas = []
+    if request.method == "GET":
+        order = request.GET.get("order")
+        if not order == 'asc':
+            sample_id = '-sample_id'
+        else:
+            sample_id = "sample_id"
+        offset = int(request.GET.get("offset"))
+        limit = int(request.GET.get("limit"))
+        datas = CommissionSheet.objects.order_by(sample_id).values("sample_id", "company_name", "project_name")[
+                offset:offset + limit]
+    total = CommissionSheet.objects.count()
+    rows = []
+    for data in datas:
+        rows.append(data)
+
+    result = {"total": total, "rows": rows}
+    return HttpResponse(json.dumps(result))
+
+
 @csrf_exempt  # 增加装饰器，作用是跳过 csrf 中间件的保护
 def LoadData(request):
-
     AllProf = Professors.objects.all()
     data = json.loads(serializers.serialize('json', AllProf))
     result = []
@@ -91,14 +139,14 @@ def LoadData(request):
         prof = item['fields']
         prof['id'] = item['pk']
         result.append(prof)
-    re = {"data":result,"biaotou":len(result)}
+    re = {"data": result, "biaotou": len(result)}
     return HttpResponse(json.dumps(re))
 
 
-@csrf_exempt#加载钢材拉力试验原始记录数据
+@csrf_exempt  # 加载钢材拉力试验原始记录数据
 def loadOriginalRecord(request):
     data = {}
-    data["weituodanbianhao"]="00000"
+    data["weituodanbianhao"] = "00000"
     data["shiyanrenyuan"] = "00000"
     data["shiyankaishi"] = "00000"
     data["shiyanjieshu"] = "00000"
@@ -108,24 +156,17 @@ def loadOriginalRecord(request):
     data["jianyanyiju"] = "00000"
     data["shebeibianhao"] = "00000"
     data["liangjubianhao"] = "00000"
-
     result = {}
     result["statues"] = 0
     result["data"] = data
     return HttpResponse(json.dumps(result))
 
 
-
-
-
-def upImages(request,paraID):
+def upImages(request, paraID):
     if request.method == "GET":
         # proName = request.GET['name']
         proId = paraID
-        #print(proId)
-        return render(request, 'zhongye/upImages.html', {'proId':proId})
+        # print(proId)
+        return render(request, 'zhongye/upImages.html', {'proId': proId})
     else:
         return HttpResponse(request, "<script>alert('图片上传出现问题！')</script>")
-
-
-
